5d7616c77f8f6436406b88e6779f036d5989cfcd
João Davi Ribeiro <81536521+JoaoDaviRibeiro@users.noreply.github.com>
2024-09-22 01:04:50 -0300

    Create SQRTest.c

---
+++ src/test/resources/cCode/stdlib/SQRTest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x;
+    double y;
+    double z;
+    double w;
+
+    w = 196.0;
+    x = 10.0;
+
+
+    y = x * x;  
+    z = w * w;  
+
+    printf("y (SQR(x)): %f\n", y);
+    printf("z (SQR(w)): %f\n", z);
+
+    return 0;
+}
